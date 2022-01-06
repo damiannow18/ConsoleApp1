51ed1e8fbea99feba069608f009d5741816afa88
damiannow18 <73836865+damiannow18@users.noreply.github.com>
2022-01-06 16:00:23 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,171 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+public static class ArrayExtensions
+{
+    public static IEnumerable<IEnumerable<T>> Split<T>(this T[] array, int size)
+    {
+        for (var i = 0; i < (float)array.Length / size; i++)
+        {
+            yield return array.Skip(i * size).Take(size);
+        }
+    }
+}
+public class ExThread
+{
+
+    // Non-static method
+    public void mythread1(int thread_number, double[] x_table)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        System.Threading.Thread.Sleep(1000 * thread_number);
+        Console.WriteLine();
+
+        for (int i = 0; i < x_table.Length; i++)
+        {
+
+            Console.Write(x_table[i] + ",");
+        }
+        
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+
+
+        Console.WriteLine("thread number: " + (thread_number + 1) + "  time: " + elapsedMs);
+    }
+}
+
+// Driver Class
+public class GFG
+{
+
+    // Main Method
+    public static void Main()
+    {
+        // Creating object of ExThread class
+        int n = 100;
+        int N = 100;
+        double a = -1;
+        double b = 1;
+        double h = (b - a) / N;
+
+        double Fx0 = 0;
+        double Fxn = 0;
+        decimal Result = 0;
+        int thread_number = 10;
+
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        // the code that you want to measure comes here
+
+
+        double Chebyshev(double x)
+        {
+            double T1, T2, T3;
+            double F = 0;
+
+            T1 = 1;
+            T2 = x;
+            T3 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                T3 = 2 * x * T2 - T1;
+                T1 = T2;
+                T2 = T3;
+            }
+            F = Math.Sqrt(Math.Sqrt(Math.Abs(T3)));
+
+
+
+            return F;
+        }
+
+        double[] create_table(double a, double b, int N)
+        {
+            double[] array = new double[N - 2];
+            double delta = (b - a) / (N - 1);
+
+            array[0] = a + delta;
+
+            for (int i = 0; i < N - 3; i++)
+            {
+                array[i + 1] = array[i] + delta;
+            }
+
+            return array;
+
+        }
+
+      
+
+       
+
+        double Fx = Chebyshev(3);
+
+        double[] Table = new double[N - 2];
+        Table = create_table(a, b, N);
+        var listOfSplitArray = Table.Split((N/(thread_number)));
+
+        Console.WriteLine(listOfSplitArray.Count());
+
+ 
+
+        foreach(var element in listOfSplitArray)
+        {
+            element.ToArray();
+            foreach(var element2 in element)
+            {
+                Console.WriteLine(element2);
+            }
+            Console.WriteLine("The end of current array");
+        }
+
+
+
+        Fx0 = h / 2 * Chebyshev(a);
+        Fxn = h / 2 * Chebyshev(b);
+
+        for (int i = 0; i < N - 2; i++)
+        {
+            Result = Result + System.Convert.ToDecimal(h * Chebyshev(Table[i]));
+        }
+
+        Result = Result + System.Convert.ToDecimal(Fx0 + Fxn);
+
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+
+        Console.WriteLine("Result: " + Result);
+        Console.WriteLine("time: " + elapsedMs);
+
+        // Console.Write(divided_tables);
+
+        ExThread obj = new ExThread();
+
+
+        Thread[] threads_array = new Thread[thread_number-1];
+        for (var i = 0; i < threads_array.Length; i++)
+        {
+            threads_array[i] = new Thread(() => obj.mythread1(i, listOfSplitArray.ElementAt(i).ToArray()));
+            threads_array[i].Start();
+        }
+
+        for (int i = 0; i < threads_array.Length; i++)
+        {
+            threads_array[i].Join();
+        }
+        // Creating thread
+        // Using thread class
+
+        Console.WriteLine("joined add threads");
+    }
+
+
+
+    
+}
